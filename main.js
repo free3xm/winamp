e29e9cabc1ad8ee3f698c3116a735a34f8a33e3e
free3 <free3xm@gmail.com>
2019-10-15 19:16:53 +0300

    up

--- main.js
+++ main.js
@@ -8,12 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
         btnsNext = document.querySelectorAll(".btnNext, .playlistNext"),
         playlist = document.querySelector(".playlist"),
         timeLine = document.querySelector(".timeline"),
-        musicBar = document.querySelector(".header_music_bar"),
+        musicBar = document.querySelector(".header_music_bar span"),
         displayTime = document.querySelectorAll(".display_time, .menu_music_time"),
         playlistAllTime = document.querySelectorAll(".menu_time_song, .menu_time_playlist"),
         displayMusicInfo = document.querySelectorAll(".display_kbps, .display_khz"),
         volume = document.querySelector(".volume"),
-        balanceSlider = document.querySelector(".changeSide");
+        balanceSlider = document.querySelector(".changeSide"),
+        musicQuality = document.querySelectorAll(".music_mono, .music_stereo"),
+        playerState = document.querySelector(".fa");
 
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
@@ -26,9 +28,16 @@ document.addEventListener("DOMContentLoaded", () => {
         listTime: 0
       },
       current = 0,
+      timer,
+      state = {
+        play: "fa-play",
+        pause: "fa-pause",
+        stop: "fa-stop"
+      },
       arrMusicSize = [],
       listOfSource = new WeakMap(),
       listOfSongs  = [];
+
       gainNode.connect(pannerNode);
       pannerNode.connect(context.destination);
       gainNode.connect(context.destination);
@@ -43,19 +52,19 @@ document.addEventListener("DOMContentLoaded", () => {
     let ul = document.createElement("ul");
     ul.classList.add("playlist_list");
 
-    arr.forEach( e => {
+    arr.forEach( (e,i) => {
       let li = document.createElement("li"),
           songName = document.createElement("div"),
           songDuration = document.createElement("div");
       li.classList.add("playlist_item");
       songName.classList.add("playlist_songName");
       songDuration.classList.add("playlist_songTime");
-      songName.textContent = e.textContent;
+      songName.textContent = `${i+1}. ${e.textContent}`;
 
       e.addEventListener("loadedmetadata", () => {
             playlistTime.listTime += e.duration;
-            playlistAllTime[1].textContent = countPlaylistTime(playlistTime.listTime);
-        songDuration.textContent = countPlaylistTime(e.duration);
+            playlistAllTime[1].textContent = countMusicTime(playlistTime.listTime);
+        songDuration.textContent = countMusicTime(e.duration);
       });
       li.appendChild(songName);
       li.appendChild(songDuration);
@@ -65,7 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
     playlist.appendChild(ul);
   }
 
-   function countPlaylistTime(counter){
+   function countMusicTime(counter){
     let firstNumber =  Math.floor(counter/60),
         secondNumber =  Math.floor(counter%60);
         firstNumber = firstNumber < 10 ? "0" + firstNumber : firstNumber;
@@ -80,7 +89,6 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
     list[index].classList.add("current_song");
-
   }
 
   function playClickedSong(arr, index, list){
@@ -94,9 +102,7 @@ document.addEventListener("DOMContentLoaded", () => {
 // functions for music time
   function musicPreviousSetup(arr, index){
     arr[index].addEventListener("loadedmetadata", () => {
-      timeLine.max = arr[index].duration;
-      playlistAllTime[0].textContent = countPlaylistTime(arr[index].duration);
-      displayMusicInfo[0].textContent = Math.floor(arrMusicSize[index]*8/arr[index].duration/1000);
+      musicSetSettings(arr, index);
     });
       timeLine.value = 0;
       displayMusicInfo[1].textContent = 44;
@@ -104,18 +110,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function musicSetSettings(arr, index){
     timeLine.max = arr[index].duration;
-    playlistAllTime[0].textContent = countPlaylistTime(arr[index].duration);
+    playlistAllTime[0].textContent = countMusicTime(arr[index].duration);
     displayMusicInfo[0].textContent = Math.floor(arrMusicSize[index]*8/arr[index].duration/1000);
+    musicBar.textContent = `${index+1}. ${arr[index].textContent} (${countMusicTime(arr[index].duration)})`;
   }
+
   function timeLineHandler(){
     timeLine.value = this.currentTime;
+  }
+
+  function displayTimer(){
     displayTime.forEach(e => {
-      e.textContent = countPlaylistTime(this.currentTime);
+      e.textContent = countMusicTime(this.currentTime);
     });
   }
 
+  timeLine.addEventListener("mousedown", () => {
+    listOfSongs[current].removeEventListener("timeupdate", timeLineHandler, false);
+  });
+
   timeLine.addEventListener("change", () => {
     listOfSongs[current].currentTime = timeLine.value;
+    listOfSongs[current].addEventListener("timeupdate", timeLineHandler, false);
   });
 
   // music volume
@@ -123,36 +139,93 @@ document.addEventListener("DOMContentLoaded", () => {
     panner.pan.value = balanceSlider.value;
   }
 
-  function balance(panner){
-    panner.pan.value = balanceSlider.value;
-  }
-  
   function volumeGain(gain){
    gain.gain.value = volume.value/100 ;
   }
 
-  volume.addEventListener("input", () => volumeGain(gainNode));
+  balanceSlider.addEventListener("input", () => {
+    let side = balanceSlider.value < 0 ? `${-balanceSlider.value *100 }% LEFT`:
+              balanceSlider.value > 0 ? `${balanceSlider.value *100 }% RIGHT`: "CENTER";
+    musicBar.classList.remove("musicBarScroll");
+    musicBar.textContent = `Balance: ${side}`;
+    return balanceOutput(pannerNode);
+    }
+  );
+  balanceSlider.addEventListener("mouseup", () => {
+    musicBar.classList.add("musicBarScroll");
+    if(listOfSongs.length > 0){
+      musicBar.textContent = `${current+1}. ${listOfSongs[current].textContent} (${countMusicTime(listOfSongs[current].duration)})`;
+    }else {
+      musicBar.textContent = "";
+    }
+  });
 
+  volume.addEventListener("input", () => {
+    musicBar.classList.remove("musicBarScroll");
+    musicBar.textContent = `VOLUME: ${+volume.value + 100}%`;
+    return volumeGain(gainNode);
+  });
+
+  volume.addEventListener("mouseup", () => {
+    musicBar.classList.add("musicBarScroll");
+    if(listOfSongs.length > 0){
+      musicBar.textContent = `${current+1}. ${listOfSongs[current].textContent} (${countMusicTime(listOfSongs[current].duration)})`;
+    }else {
+      musicBar.textContent = "";
+    }
+  });
+
+  function checkQuality(){
+    if(pannerNode.channelCount > 1 ){
+      musicQuality[1].classList.add("activeQuality");
+    } else {
+      musicQuality[0].classList.add("activeQuality");
+    }
+
+  }
   // functions for buttons of  music control
+  function stateHandlerVisual(currentState){
+    for(let key in state){
+      playerState.classList.remove(state[key]);
+      console.log(state[key])
+    }
+    playerState.classList.add(currentState);
+    console.log(currentState)
+  }
+
   function play(arr, index, list){
     if(arr.length > 0){
       balanceOutput(pannerNode, arr[index]);
       musicPreviousSetup(arr, index);
       showSong(list, index);
-      musicBar.textContent = `${index+1}. ${arr[index].textContent}`;
-    volumeGain(gainNode);
-    balanceSlider.addEventListener("input", () => balance(pannerNode));
+      volumeGain(gainNode);
 
-    listOfSource.get(arr[index]).connect(context.destination);
-    listOfSource.get(arr[index]).connect(gainNode);
-    listOfSource.get(arr[index]).connect(pannerNode);
-
-    arr[index].play();
+      listOfSource.get(arr[index]).connect(context.destination);
+      listOfSource.get(arr[index]).connect(gainNode);
+      listOfSource.get(arr[index]).connect(pannerNode);
+      stateHandlerVisual(state.play);
+      timer ? clearTimeout(timer) : false;
+      checkQuality();
+      arr[index].play();
     }
   }
 
   function pause(arr,index){
-    arr.length > 0 ? arr[index].pause() : false;
+    if(arr.length > 0){
+       arr[index].pause();
+       stateHandlerVisual(state.pause);
+       let value = displayTime[0].textContent;
+       timer = setInterval(() => {
+            displayTime.forEach(e => {
+              e.textContent = `  :  `;
+            });
+        setTimeout(() => {
+             displayTime.forEach(e => {
+               e.textContent = value;
+             });
+       } , 1000);
+     },2000);
+    }
   }
 
   function stop(arr, index){
@@ -160,6 +233,9 @@ document.addEventListener("DOMContentLoaded", () => {
       arr[index].pause();
       arr[index].currentTime = 0;
       timeLineHandler(0);
+      stateHandlerVisual(state.stop);
+      timer ? clearTimeout(timer) : false;
+      musicQuality.forEach(e => e.classList.remove("activeQuality"));
     }
   }
 
@@ -194,10 +270,10 @@ document.addEventListener("DOMContentLoaded", () => {
     if(arr.length != i){
       play(arr, i, list);
       current = i;
+      musicSetSettings(arr, i);
     } else {
       play(arr, 0, list);
     }
-
   }
   function clearPlaylist(arr, index, list){
     console.log(arr , index);
@@ -224,14 +300,17 @@ document.addEventListener("DOMContentLoaded", () => {
     arrMusicSize= [];
 
     [].forEach.call(this.files, (e,i) => {
-      arrMusicSize.push(this.files[i].size);
       let song = new Audio(this.files[i].name);
         song.preload = "metadata";
         song.textContent = this.files[i].name;
         song.type = this.files[i].type;
         song.src = URL.createObjectURL(this.files[i]);
+
         song.addEventListener("ended", () => onEnd(listOfSongs, i+1, playlistMusicList), false);
         song.addEventListener("timeupdate", timeLineHandler, false);
+        song.addEventListener("timeupdate", displayTimer, false);
+
+        arrMusicSize.push(this.files[i].size);
         listOfSongs.push(song);
         let source = context.createMediaElementSource(song);
         listOfSource.set(song, source);
@@ -241,6 +320,5 @@ document.addEventListener("DOMContentLoaded", () => {
     playlistMusicList = document.querySelectorAll(".playlist_item");
     playClickedSong(listOfSongs, current, playlistMusicList);
     play(listOfSongs, current, playlistMusicList);
-
   });
 });
